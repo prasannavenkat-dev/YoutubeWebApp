2dafa604e2dd2aab202f371f8972a82c796951e2
prasannavenkat-dev <74189369+prasannavenkat-dev@users.noreply.github.com>
2021-02-19 21:18:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function updateSigninStatus() {
 
 getDetails();
 async function getDetails(){
- let url = 'https://youtube.googleapis.com/youtube/v3/subscriptions?key=AIzaSyBeGyvz-cxF-FFCADJNNxcJozZd6361UqA&part=snippet&mine=true'
+ let url = 'https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&key=AIzaSyBeGyvz-cxF-FFCADJNNxcJozZd6361UqA'
  let getResults = await fetch(url);
   let res = await getResults.json();
   console.log(res);
