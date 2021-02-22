c4f50a4759e5f8ced0e047d3ad08a5be4931cf8f
prasannavenkat-dev <74189369+prasannavenkat-dev@users.noreply.github.com>
2021-02-22 19:41:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,60 +88,7 @@ async function getDetails(){
  let getResults = await fetch(url);
   let res = await getResults.json();
      
-    for(i=0;i<res.length;i++){
-  var container = document.createElement('div');
-  container.setAttribute('class','container-fluid');
-  document.body.append(container);
-  
-  
-          var card = document.createElement('div');
-          card.setAttribute('class','card');
-           container.append(card);
-               
-           var row = document.createElement('div');
-           row.setAttribute('class','row');
-            card.append(row);
-           
-            var imgSec = document.createElement('div');
-            imgSec.setAttribute('class','col-md-4');    
-             row.append(imgSec);
-                //img
-                 var img = document.createElement('img');
-                 img.setAttribute('src',res.items[i].snippet.thumbnails.default.url);
-                 imgSec.append(img);
-         
-             var details = document.createElement('div');
-             details.setAttribute('class','col-md-8');
-              row.append(details);
-  
-  
-              var cardBlock = document.createElement('div');
-              cardBlock.setAttribute('class','card-block');
-               details.append(cardBlock);
-                 
-                
-               //TITLE
-                   
-               var title = document.createElement('h4');
-               title.setAttribute('class','card-title');
-               title.innerHTML=res.items[i].snippet.title;
-               cardBlock.append(title);
-  
-                 //channel 
-                 var channelName = document.createElement('h6');
-                 channelName.setAttribute('class','card-title');
-                 channelName.innerHTML=res.items[i].snippet.channelTitle;
-                 cardBlock.append(channelName);
-                  
-                //description
-  
-                var description = document.createElement('p');
-                description.setAttribute('class','card-text');
-                description.innerHTML=res.items[i].snippet.description;
-                 cardBlock.append(description);
-  
-  
-}
+ console.log(res)
 
 }
 
